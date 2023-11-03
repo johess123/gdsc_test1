55d5af81e0a69cd47049b509e124d699ee83ff1f
johess123 <104426729+johess123@users.noreply.github.com>
2023-11-03 12:57:37 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,10 @@
 # 先導入後面會用到的套件
 import requests # 請求工具
 from bs4 import BeautifulSoup # 解析工具
-import datetime # 取得日期
 import time # 用來暫停程式
 
 # 要爬的股票
 stock = ["1101","2330"]
-# 取得現在時間
-date = datetime.datetime.today().strftime("%Y%m%d")
 for i in range(len(stock)): # 迴圈依序爬股價
     # 現在處理的股票
     stockid = stock[i]
